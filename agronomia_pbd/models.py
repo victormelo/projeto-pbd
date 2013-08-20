be09645baca06ba955fc65e1243ab70873573b4c
victormelo <victorklbr@gmail.com>
2013-08-19 21:33:25 -0300

    Inicio da criação dos templates e conserto do erro de danyllo: é length!!

--- agronomia_pbd/models.py
+++ agronomia_pbd/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-#testando push
-
 class Usuario(models.Model):
     nome = models.CharField(max_length = 80)
     email = models.CharField(max_length = 80)
@@ -11,22 +8,22 @@ class Usuario(models.Model):
     instituicao = models.CharField(max_length = 80)
     observacao = models.CharField(max_length = 80)
     cidade = models.CharField(max_length = 80)
-    fazenda = models.CharField(max_lenght = 80)
-    estado = models.CharField(max_lenght = 80)
-    rua = models.CharField(max_lenght = 80)
+    fazenda = models.CharField(max_length = 80)
+    estado = models.CharField(max_length = 80)
+    rua = models.CharField(max_length = 80)
     
 class Experimento(models.Model):
     usuario = models.ForeignKey(Usuario)
-    nome = models.CharField(max_lenght = 80)
+    nome = models.CharField(max_length = 80)
 
+class Instrumento(models.Model):
+    nome = models.CharField(max_length = 80)
+    tipo = models.CharField(max_length = 80)
+    
 class Experimento_Instrumentos(models.Model):
     instrumentos = models.ForeignKey(Instrumento)
     experimento = models.ForeignKey(Experimento)
 
-class Instrumento(models.Model):
-    nome = models.CharField(max_lenght = 80)
-    tipo = models.CharField(max_lenght = 80)
-    
 class Localizacao(models.Model):
     experimento = models.ForeignKey(Experimento)
     latitude = models.FloatField()
@@ -35,15 +32,11 @@ class Localizacao(models.Model):
     
 class Tratamento(models.Model):
     experimento = models.ForeignKey(Experimento)
-    nome = models.CharField(max_lenght = 80)
-    descricao = models.CharField(max_lenght = 80)
-    
-class Experimento_Solo(models.Model):
-    experimento = models.ForeignKey(Experimento)
-    solo = models.ForeignKey(Solo)
+    nome = models.CharField(max_length = 80)
+    descricao = models.CharField(max_length = 80)
     
 class Solo(models.Model):
-    nome = models.CharField(max_lenght = 80)
+    nome = models.CharField(max_length = 80)
     diametro = models.FloatField()
     frequencia = models.FloatField()
     argila = models.FloatField()
@@ -53,18 +46,7 @@ class Solo(models.Model):
     lamina = models.FloatField()
     densidade = models.FloatField()
     teorUmidade = models.FloatField()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+class Experimento_Solo(models.Model):
+    experimento = models.ForeignKey(Experimento)
+    solo = models.ForeignKey(Solo)
